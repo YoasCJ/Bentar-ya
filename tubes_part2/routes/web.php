6c93471ebfc74c8c7e50fc4a906c36d10f17644e
adiva20005 <divagp12345@gmail.com>
2025-06-11 12:26:23 +0700

    apa ya

--- tubes_part2/routes/web.php
+++ tubes_part2/routes/web.php
@@ -49,11 +49,11 @@ Route::middleware('auth')->group(function () {
 });     
 
 // Rute khusus untuk Dashboard Admin
-<<<<<<< Updated upstream
+
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard'); // Membuka file tampilan yang baru saja kita buat
 })->middleware('role:admin')->name('admin.dashboard');
-=======
+
 Route::middleware('role:admin')->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'index']);
 });
@@ -71,5 +71,5 @@ Route::middleware(['role:admin'])->prefix('admin')->group(function () {
 
     // Rute untuk Manajemen Skill
     Route::get('/skills', [AdminController::class, 'skills'])->name('admin.skills');
+
 });
->>>>>>> Stashed changes
